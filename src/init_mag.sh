be4f455e9a7acf7ceea8992478967445ec4c474a
John Sundh <john.sundh@scilifelab.se>
2023-04-15 06:49:43 +0200

    Add init script

---
+++ src/init_mag.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -c
+module load bioinfo-tools Nextflow/22.10.1 nf-core nf-core-pipelines
+NXF_HOME="/proj/snic2020-5-486/nobackup/SMS-23-6668-micegut/.nextflow"
+export NXF_OPTS='-Xms1g -Xmx4g'
+export NXF_SINGULARITY_CACHEDIR="/sw/data/ToolBox/nf-core/"
+export NXF_SINGULARITY_LIBRARYDIR="/sw/bioinfo/nf-core-pipelines/latest/rackham/singularity_cache_dir"
+nextflow pull nf-core/mag -r 2.3.0
