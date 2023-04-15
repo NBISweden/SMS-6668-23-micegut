9cd9efe1dad588594d68f93bcc9e5e57778f654e
John Sundh <john.sundh@scilifelab.se>
2023-04-15 07:17:03 +0200

    Remove init script

--- src/init_mag.sh
+++
@@ -1,7 +0,0 @@
-module purge
-module load uppmax bioinfo-tools Nextflow/22.10.1
-NXF_HOME="/proj/snic2020-5-486/nobackup/SMS-23-6668-micegut/.nextflow"
-export NXF_OPTS='-Xms1g -Xmx4g'
-export NXF_SINGULARITY_CACHEDIR="/sw/data/ToolBox/nf-core/"
-export NXF_SINGULARITY_LIBRARYDIR="/sw/bioinfo/nf-core-pipelines/latest/rackham/singularity_cache_dir"
-nextflow pull nf-core/mag -r 2.3.0
