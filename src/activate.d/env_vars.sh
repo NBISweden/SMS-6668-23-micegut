dbbf023f617a2aabe58d199997b6bdc4f52a2cc8
John Sundh <john.sundh@scilifelab.se>
2023-04-15 08:29:48 +0200

    Add env vars scripts

---
+++ src/activate.d/env_vars.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export NXF_HOME="/proj/snic2020-5-486/nobackup/SMS-23-6668-micegut/.nextflow"
+export NXF_OPTS='-Xms1g -Xmx4g'
+export NXF_SINGULARITY_CACHEDIR="/sw/data/ToolBox/nf-core/"
+export NXF_SINGULARITY_LIBRARYDIR="/sw/bioinfo/nf-core-pipelines/latest/rackham/singularity_cache_dir"
